63eb6afe918c96a9b6760a7da07943bb37ed524c
Vadim Kleiman <vadim.kleiman@gmail.com>
2018-04-12 18:19:19 +0300

    Implement save/load image to/from database

--- app/src/main/java/ru/spbau/mit/dictionary/AddWordActivity.kt
+++ app/src/main/java/ru/spbau/mit/dictionary/AddWordActivity.kt
@@ -24,8 +24,15 @@ import picture.bing.api.com.PicturesDescription
 import ru.spbau.mit.data.DictionaryContract
 import ru.spbau.mit.data.DictionaryProvider
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.util.Log
+import com.squareup.picasso.Target
+import ru.spbau.mit.dictionary.R.string.from
 import java.io.ByteArrayOutputStream
+
+import java.lang.Thread.sleep
 import java.util.concurrent.locks.Condition
 import java.util.concurrent.locks.Lock
 import java.util.concurrent.locks.ReentrantLock
@@ -70,54 +77,57 @@ class AddWordActivity : AppCompatActivity() {
         }
     }
 
-    internal inner class FindPicture(private val sharedText: String,
-                                     private val syncObj : Lock,
-                                     private val condObj : Condition) : AsyncTask<Void, Void?, PicturesDescription?>() {
+    internal inner class FindPicture(private val sharedText: String) : AsyncTask<Void, Void?, PicturesDescription?>() {
         override fun doInBackground(vararg p0: Void?): PicturesDescription? {
             return try {
-                bingPicture.execute(sharedText, 10)
+                Log.d("WOW2", "Start")
+                var out = bingPicture.execute(sharedText, 10)
+                Log.d("WOW2", "End")
+                out
             } catch (e : Exception) {
                 null
             }
         }
 
-
-//        override fun onPreExecute() {
-//            super.onPreExecute()
-//        }
-
         override fun onPostExecute(result: PicturesDescription?) {
-            super.onPostExecute(result)
-            syncObj.lock()
-            try {
-                val imgView = findViewById<ImageView>(R.id.wordImage)
-                val urlList = result!!.contentUrlList
-                Picasso.with(this@AddWordActivity)
-                        .load(urlList[0])
-                        .resize(width, height)
-                        .into(imgView)
-                val bitmap = (imgView.drawable as BitmapDrawable).bitmap
-                val baos = ByteArrayOutputStream()
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
-                imageInByte = baos.toByteArray()
-                condObj.signal()
-            } finally {
-                syncObj.unlock()
-            }
+            val imgView = findViewById<ImageView>(R.id.wordImage)
+            val urlList = bingPicture.execute(sharedText, 10).contentUrlList
+            Picasso.with(this@AddWordActivity)
+                    .load(urlList[0])
+                    .resize(width, height)
+                    .into(imgView)
         }
     }
 
     internal inner class TranslateTask(private val sharedText: String) : AsyncTask<Void, Void?, WordDescription?>() {
         private val progressBar = findViewById<ProgressBar>(R.id.progressbar)
-
+        private lateinit var urlList : List<String>
+        private var imageInByte : ByteArray? = null
+        private var bitmap : Bitmap? = null
+        private var isLoadPicture : Boolean = false
         override fun onPreExecute() {
             super.onPreExecute()
             progressBar.visibility = View.VISIBLE
+            contentResolver.query(
+                    DictionaryProvider.CONTENT_WORDS_ENTRY,
+                    null,
+                    "${DictionaryContract.WordsEntry.COLUMN_NAME} = '$sharedText'",
+                    null, null).use { if (it.moveToNext()) {
+                imageInByte = it.getBlob(it.getColumnIndex(DictionaryContract.WordsEntry.COLUMN_IMAGE))
+                isLoadPicture = true
+            } }
         }
 
         override fun doInBackground(vararg params: Void): WordDescription? {
+            if (!isLoadPicture)
+                urlList = bingPicture.execute(sharedText, 10).contentUrlList
+//            val bitmap = (imgView.drawable as BitmapDrawable).bitmap
+//            val baos = ByteArrayOutputStream()
+//            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+//            imageInByte = baos.toByteArray()
             return try {
                 translate.execute(sharedText, Language.ENGLISH, Language.RUSSIAN)
+
             } catch (e: Exception) {
                 null
             }
@@ -127,30 +137,51 @@ class AddWordActivity : AppCompatActivity() {
             super.onPostExecute(result)
             progressBar.visibility = View.INVISIBLE
             val view = findViewById<TextView>(R.id.wordTextView)
-            val syncObj : Lock = ReentrantLock()
-            val condObj : Condition = syncObj.newCondition()
-            val findPicture = FindPicture(sharedText, syncObj, condObj)
-            findPicture.execute()
+            val imgView = findViewById<ImageView>(R.id.wordImage)
+            var t : com.squareup.picasso.Target = object : com.squareup.picasso.Target {
+                override fun onBitmapFailed(errorDrawable: Drawable?) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+                    this@TranslateTask.bitmap = bitmap
+                    imgView.setImageBitmap(bitmap)
+                }
+
+                override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+
+                }
+
+            }
+            if (!isLoadPicture) {
+                Log.d("LOAD", "NO")
+                Picasso.with(this@AddWordActivity)
+                        .load(urlList[0])
+                        .resize(width, height)
+                        .into(t)
+            }
+            else {
+                Log.d("LOAD", "YES")
+                val decodeByteArray = BitmapFactory.decodeByteArray(imageInByte, 0, imageInByte!!.size)
+                imgView.setImageBitmap(decodeByteArray)
+            }
             if (result != null && !result.ranking.isEmpty()) {
 
                 view.text = "$sharedText -> ${result.ranking.first { it.text != null }.translation.first { it.text != null }.text}"
                 val saveButton = findViewById<Button>(R.id.saveWord)
                 saveButton.visibility = View.VISIBLE
-                syncObj.lock()
-                try {
-                    while (imageInByte == null) {
-                        condObj.await()
-                    }
-                } finally {
-                    syncObj.unlock()
-                }
                 saveButton.setOnClickListener {
                     val values = ContentValues()
                     values.put(DictionaryContract.WordsEntry.COLUMN_NAME, sharedText)
                     values.put(DictionaryContract.WordsEntry.COLUMN_HIDDEN, false)
                     values.put(DictionaryContract.WordsEntry.COLUMN_STATE, DictionaryContract.WordsEntry.STATE_ON_LEARNING)
                     values.put(DictionaryContract.WordsEntry.COLUMN_PRIORITY, 0)
-                    values.put(DictionaryContract.WordsEntry.COLUMN_IMAGE, imageInByte)
+                    if (this@TranslateTask.bitmap != null) {
+                        val baos = ByteArrayOutputStream()//and then I convert that image into a byteArray
+                        this@TranslateTask.bitmap!!.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+                        imageInByte = baos.toByteArray()
+                        values.put(DictionaryContract.WordsEntry.COLUMN_IMAGE, imageInByte)
+                    }
                     try {
                         contentResolver.insert(DictionaryProvider.CONTENT_WORDS_ENTRY, values)
                     } catch (e: Exception) {
