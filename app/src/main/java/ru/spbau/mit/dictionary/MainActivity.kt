38f04d5e2abc3dd05e8f7ea89109f90ea3c542f8
YorovSobir <sobir.yorov94@gmail.com>
2018-04-17 23:08:41 +0300

    remove unused menu items

--- app/src/main/java/ru/spbau/mit/dictionary/MainActivity.kt
+++ app/src/main/java/ru/spbau/mit/dictionary/MainActivity.kt
@@ -13,10 +13,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
-import ru.spbau.mit.data.DictionaryContract
-import ru.spbau.mit.data.DictionaryProvider
 import ru.spbau.mit.dictionary.main.PagerAdapter
-import ru.spbau.mit.dictionary.main.Word
 import ru.spbau.mit.dictionary.study.StudyActivity
 
 
@@ -25,13 +22,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val rootView = findViewById<View>(R.id.appbar)
         val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
         val tabLayout = findViewById<View>(R.id.tab_layout) as TabLayout
-//        tabLayout.addTab(tabLayout.newTab().setText("На изучение"))
-//        tabLayout.addTab(tabLayout.newTab().setText("Изученные"))
         tabLayout.tabGravity = TabLayout.GRAVITY_FILL
 
         val viewPager = findViewById<View>(R.id.pager) as ViewPager
@@ -51,64 +45,48 @@ class MainActivity : AppCompatActivity() {
 
             }
         })
-
-
-
-//        val clipboard = this.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-//
-//        val mPrimaryChangeListener = OnPrimaryClipChangedListener {
-//            val clipBoard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-//            val item = clipBoard.primaryClip.getItemAt(0)
-//            if (item.text != null) {
-//                val intent = Intent(baseContext, AddWordActivity::class.java)
-//                intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT or Intent.FLAG_ACTIVITY_NEW_TASK)
-//                intent.putExtra("word", item.text.toString())
-//                baseContext.startActivity(intent)
-//            }
-//        }
-//        clipboard.addPrimaryClipChangedListener(mPrimaryChangeListener)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val menuInflater = menuInflater
         menuInflater.inflate(R.menu.menu, menu)
         //getting the search view from the menu
-        val searchViewItem = menu!!.findItem(R.id.menuSearch)
-
-        //getting search manager from systemservice
-        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
-
-        //getting the search view
-        val searchView = searchViewItem.actionView as SearchView
-
-        //you can put a hint for the search input field
-        searchView.queryHint = "Поиск слов..."
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
-
-        //by setting it true we are making it iconified
-        //so the search input will show up after taping the search iconified
-        //if you want to make it visible all the time make it false
-        searchView.setIconifiedByDefault(true)
-
-        //here we will get the search query
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String): Boolean {
-                // TODO("do search here")
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String): Boolean {
-                return false
-            }
-        })
+//        val searchViewItem = menu!!.findItem(R.id.menuSearch)
+//
+//        //getting search manager from system telservice
+//        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+//
+//        //getting the search view
+//        val searchView = searchViewItem.actionView as SearchView
+//
+//        //you can put a hint for the search input field
+//        searchView.queryHint = "Поиск слов..."
+//        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+//
+//        //by setting it true we are making it iconified
+//        //so the search input will show up after taping the search iconified
+//        //if you want to make it visible all the time make it false
+//        searchView.setIconifiedByDefault(true)
+//
+//        //here we will get the search query
+//        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+//            override fun onQueryTextSubmit(query: String): Boolean {
+//                // TODO("do search here")
+//                return false
+//            }
+//
+//            override fun onQueryTextChange(newText: String): Boolean {
+//                return false
+//            }
+//        })
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
         when (item.itemId) {
-            R.id.menuAbout -> Toast.makeText(this, "You clicked about", Toast.LENGTH_SHORT).show()
-            R.id.menuSettings -> Toast.makeText(this, "You clicked settings", Toast.LENGTH_SHORT).show()
+//            R.id.menuAbout -> Toast.makeText(this, "You clicked about", Toast.LENGTH_SHORT).show()
+//            R.id.menuSettings -> Toast.makeText(this, "You clicked settings", Toast.LENGTH_SHORT).show()
             R.id.menuTest -> {
                 val intent = Intent(this, StudyActivity::class.java)
                 startActivity(intent)
@@ -117,35 +95,3 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 }
-//import android.app.Activity
-//import android.content.Intent
-//import android.os.Bundle
-//import android.support.v7.app.AppCompatActivity
-//import android.util.Log
-//import android.view.View
-//
-//
-//class MainActivity : AppCompatActivity(), View.OnClickListener {
-//
-//    private var globalContext: GlobalContext? = null
-//    private val REQUEST_CODE = 1
-//
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE && data != null) {
-//            globalContext!!.setLanguage(data)
-//        }
-//    }
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//        Log.d("OnCreate", "Start")
-//        globalContext = GlobalContext(this, this)
-//        globalContext!!.onCreate()
-//    }
-//
-//    override fun onClick(v: View) {
-//        globalContext!!.startActivityForResult(SearchActivity::class.java, REQUEST_CODE)
-//    }
-//}
